937ee1eaf6ff0024241144393afc7d9ff67ae189
santoshlearner07 <89893625+santoshlearner07@users.noreply.github.com>
2023-08-20 20:22:31 +0530

    All data at one place

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Header from './fixed/Header';
+import Home from "./component/Home"
+import Services from "./component/Services"
+import About from './component/About';
+import ContactUs from './component/ContactUs'
+import Footer from './fixed/Footer';
+import PageNotFound from './fixed/PageNotFound';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        <Routes>
+          <Route path='/' element={<Home />}  />
+          <Route path='/services' element={<Services />}  />
+          <Route path='/about' element={<About />}  />
+          <Route path='/contact' element={<ContactUs />}  />
+          <Route path='*' element={<PageNotFound />}  />
+        </Routes>
+        <Footer />
     </div>
   );
 }
